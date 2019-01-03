b43272d1baf78b86dd0aa38a9a95d225bf66f701
Sz. Balázs <szaboblu@gmail.com>
2019-01-03 20:23:39 +0100

    megírtuk az oldalak összahasonlítását, belekezdtünk az algoritmus szerkezetébe

--- Beadandó/Beadandó/main.cpp
+++ Beadandó/Beadandó/main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <list>
 
 using namespace std;
 // Eképzelt működés  eddig:
@@ -33,7 +34,7 @@ struct Vonal{
 }
     
     void belsoforgat(int m){ // ide kell rakni eg overflowt, mert ugye ki és be is max 4 lehet
-        if(ki + m > 4 || be + m > 4 ){
+        if(ki + m > 4 || be + m > 4 ){ // ezt meg kell írni mert más lett az input
             ki = ki + m - 4;
             be = be + m - 4;
         }
@@ -52,20 +53,22 @@ struct Kep {
     //map<Vonal, vector<int>> szinek; // melyik vonal milyen szin (mi legyen string vagy int (itt csak sima összeadás is lehetne az új szín) -> nem jó lila != cian ) -->>-->> RGB kód kell
     vector<Vonal> vonalak;
     map<int,vector<int>> oldalak;
-    bool forgathato = false; //alap nem
+    bool forgathato = false; //alap nem majd vegyük ki mert globális
     int forgatottsag; // 0,1,2,3 * 90° // vagy ezek többszörösei
     
 public:
     Kep(){
         forgatottsag = 0;
+        azonosito = 0;
+        //oldalak =
     }
     
-    Kep(int az, vector<Vonal> v, bool forg){
+    /*Kep(int az, vector<Vonal> v, bool forg){
         azonosito = az;
         forgatottsag = 0;
         forgathato = forg;
         vonalak = v;
-    }
+    }*/
     
     Kep(int az, vector<Vonal> v){
         azonosito = az;
@@ -74,6 +77,7 @@ public:
     }
     
     void oldaltolt(){
+        cout<< endl << azonosito << " azonosítójú" << endl;
         for(int i = 1; i <=4; i++){
             vector<int> oldali = {0,0,0}; // alapból 0 0 0;
             for(int j = 0; j < vonalak.size(); j++){
@@ -83,7 +87,7 @@ public:
                     oldali[2]+= vonalak[j].szin[2];
                 }
             }
-            cout << "oldalak: " << oldali[0] << oldali[1] << oldali[2] << endl;
+            cout << i<<". oldal: " << oldali[0] << oldali[1] << oldali[2] << endl;
             oldalak[i] = oldali;
         }
     }
@@ -103,19 +107,42 @@ public:
     }
 };
 
-void csatlakozas(){
-    
-};
+map<vector<Kep>,vector<int>> csatlakozik(Kep k1, Kep k2){
+    map<vector<Kep>,vector<int>> csatlakozasok;
+    cout << endl;
+    for(int i = 1; i <= k1.oldalak.size(); i ++){
+        //int forgat;
+        for(int j = 1; j <= k2.oldalak.size(); j++){
+            if((i + j) == 5 && k1.oldalak[i] == k2.oldalak[j]){
+                vector<Kep> kepvektor;
+                kepvektor.push_back(k1);
+                kepvektor.push_back(k2);
+                
+                csatlakozasok[kepvektor].push_back(i);
+                //k2.forgat(forgat);
+                csatlakozasok[kepvektor].push_back(j);
+                cout << kepvektor[0].azonosito << " Azonosítójú a " << i << ". oldalával csatlakozik " <<kepvektor[1].azonosito << " azonosítójú " << j << ". oldalához "<< endl;
+            }
+        }
+    }
+    return csatlakozasok;
+}
 
 struct Keszlet{
     //int pos_x,pos_y;
     //vector<Kep> Hurok; // ide menti el a hurok sorrendjét// vagy mentse máshova?
     map<Kep,int> keszlet; //melyik képből mennyi van még map<Kep,mennyi>
     
+    Keszlet(){};
+    void hozzaad(Kep k, int sz){
+        keszlet[k] = sz;
+    };
+    
     void felhasznal(Kep mit){ // felhasznál = elhasznál (melyiket tartsuk meg?)
         if(keszlet[mit] > 0){
             keszlet[mit]--;
         }
+        else keszlet.erase(mit);
     }
     
     void sorbeolvas(string sor){
@@ -138,19 +165,6 @@ struct Keszlet{
         keszlet[kep] = m;
     }
     
-    void kiir(){
-        for(auto it : keszlet){
-            cout << endl << "Azonosító: "<<it.first.azonosito<<" db: "<<it.second<< endl;
-            for(int j = 0; j < it.first.vonalak.size();j++){
-                cout <<"Vonal"<<j+1<< ": " <<it.first.vonalak[j].ki <<"->"<< it.first.vonalak[j].be <<" ["<< it.first.vonalak[j].szin[0]<<" "<< it.first.vonalak[j].szin[1] <<" "<< it.first.vonalak[j].szin[2]<<"] "<< endl;
-                
-            }
-            Kep k = it.first;
-            k.oldaltolt();
-        }
-        
-    }
-    
     void beolvas(){
         cout << "Hány féle képpel dolgozik?"<<endl;
         string M;
@@ -166,20 +180,80 @@ struct Keszlet{
             sorbeolvas(adat);
         }
     }
-};
-
-
-/*
- int utkereses(){
     
-    return utkereses();
+    void kiir(){
+        for(auto it : keszlet){
+            cout << endl << "Azonosító: "<< it.first.azonosito<<" db: "<<it.second<< endl;
+            for(int j = 0; j < it.first.vonalak.size();j++){
+                cout <<"Vonal"<<j+1<< ": " <<it.first.vonalak[j].ki <<"->"<< it.first.vonalak[j].be <<" ["<< it.first.vonalak[j].szin[0]<<" "<< it.first.vonalak[j].szin[1] <<" "<< it.first.vonalak[j].szin[2]<<"] "<< endl;
+                
+            }
+            Kep k = it.first;
+            k.oldaltolt();
+        }
+        
+    }
+    
+    /*
+    int utkereses(){
+        int hossz;
+    }*/
 };
- */
+
+struct Halo{
+    vector<list<Kep>> grafok;
+    
+    int utkereses(Keszlet k){
+        int meret = 0;
+        /*for(int i= 0; i<k.keszlet.size();i++){
+            for(int j= 0; j<k.keszlet.size();j++){
+                if(csatlakozik(k.keszlet[], <#Kep k2#>))
+            }
+        }*/
+        for(auto i : k.keszlet){
+            for(auto j: k.keszlet){
+                if(csatlakozik(i.first, j.first).size() >  0){
+                    
+                }
+            }
+        };
+        
+        return meret;
+    };
+};
 
 int main(int argc, const char * argv[]) {
+    //Keszlet keszlet;
+    //keszlet.beolvas();
+    //keszlet.kiir();
+    /*Vonal v1(2,3,0,1,0), v2(2,4,0,0,1),v3(4,3,1,0,0);
+    Kep k1(1,{v1,v2,v3});
+    k1.oldaltolt();
+    Vonal v4(1,3,0,0,1),v5(2,3,0,1,0),v6(2,4,1,0,0);
+    Kep k2(2,{v4,v5,v6});
+    k2.oldaltolt();
+     
+    Vonal h1(1,4,1,1,0),h2(2,3,0,0,1);
+    Vonal h3(1,3,0,0,1),h4(1,4,1,1,0);
+    Kep k3(3,{h1,h2}), k4(4,{h3,h4});
+    k3.oldaltolt();
+    k4.oldaltolt();
     Keszlet keszlet;
-    keszlet.beolvas();
-    keszlet.kiir();
+    keszlet.hozzaad(k3,2);
+    keszlet.hozzaad(k4,3);
+    csatlakozik(k3,k4);*/
+    
+    Vonal h1(1,2,1,1,1),h2(2,4,1,1,1);
+    Vonal h3(4,3,1,1,1),h4(1,3,1,1,1);
+    Kep k1(1,{h1}), k2(2,{h2}) , k3(3,{h3}) , k4(4,{h4});
+    k1.oldaltolt();
+    k2.oldaltolt();
+    k3.oldaltolt();
+    k4.oldaltolt();
+    Keszlet keszlet;
+    keszlet.hozzaad(k1,1);
+    keszlet.hozzaad(k2,1);keszlet.hozzaad(k3,1);keszlet.hozzaad(k4,1);
+    csatlakozik(k3,k4);
     
     // ide kell az algoritmus
     cout << "Yeah!!!!!" <<endl;
